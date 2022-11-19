80f6546e6af77e0cdfdc4811b3e915db084ee832
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2022-11-19 20:54:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myBtn").onclick = function() {msg()};
+
 function msg(){  
  alert("Hello Javatpoint");  
-}  
+} 
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
